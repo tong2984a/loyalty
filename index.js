492a15829ef7c8313d51a80fcd1863a6734112b0
Leung <andrew_leung@cable.comcast.net>
2021-05-30 04:15:36 -0400

    init

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
-const cool = require('cool-ascii-faces');
 const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const {pool} = require('./config')
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors())
+
+const getBooks = (request, response) => {
+  pool.query('SELECT * FROM books', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+const addBook = (request, response) => {
+  const {author, title} = request.body
+
+  pool.query(
+    'INSERT INTO books (author, title) VALUES ($1, $2)',
+    [author, title],
+    (error) => {
+      if (error) {
+        throw error
+      }
+      response.status(201).json({status: 'success', message: 'Book added.'})
+    },
+  )
+}
+
+app
+  .route('/books')
+  // GET endpoint
+  .get(getBooks)
+  // POST endpoint
+  .post(addBook)
+
+// Start server
+app.listen(process.env.PORT || 3002, () => {
+  console.log(`Server listening`)
+})
