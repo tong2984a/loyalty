9d3579b4bf82b6d7d0ee220be59ddde0e15f93eb
Leung <andrew_leung@cable.comcast.net>
2021-06-02 23:45:39 -0400

    trim query inputs

--- queries.js
+++ queries.js
@@ -12,7 +12,7 @@ const getUsers = (request, response) => {
 const getUserById = (request, response) => {
   const id = request.params.id
 
-  pool.query('SELECT * FROM users WHERE alias = $1', [id], (error, results) => {
+  pool.query('SELECT * FROM users WHERE alias = $1', [id.trim()], (error, results) => {
     if (error) {
       throw error
     }
@@ -22,7 +22,7 @@ const getUserById = (request, response) => {
 
 const createUser = (request, response) => {
   const { alias, address, balance } = request.body
-  pool.query('INSERT INTO users (alias, address, balance) VALUES ($1, $2, $3) returning ID', [alias, address, balance], (error, result) => {
+  pool.query('INSERT INTO users (alias, address, balance) VALUES ($1, $2, $3) returning ID', [alias.trim(), address.trim(), balance], (error, result) => {
     if (error) {
       throw error
     }
@@ -36,7 +36,7 @@ const updateUser = (request, response) => {
 
   pool.query(
     'UPDATE users SET address = $1, balance = $2 WHERE alias = $3',
-    [address, balance, id],
+    [address.trim(), balance, id.trim()],
     (error, results) => {
       if (error) {
         throw error
@@ -52,7 +52,7 @@ const earnReward = (request, response) => {
 
   pool.query(
     'UPDATE users SET balance = balance + $1 WHERE alias = $2',
-    [amount, id],
+    [amount, id.trim()],
     (error, results) => {
       if (error) {
         throw error
@@ -68,7 +68,7 @@ const redeemReward = (request, response) => {
 
   pool.query(
     'UPDATE users SET balance = balance - $1 WHERE alias = $2',
-    [amount, id],
+    [amount, id.trim()],
     (error, results) => {
       if (error) {
         throw error
@@ -81,7 +81,7 @@ const redeemReward = (request, response) => {
 const deleteUser = (request, response) => {
   const id = request.params.id
 
-  pool.query('DELETE FROM users WHERE alias = $1', [id], (error, results) => {
+  pool.query('DELETE FROM users WHERE alias = $1', [id.trim()], (error, results) => {
     if (error) {
       throw error
     }
